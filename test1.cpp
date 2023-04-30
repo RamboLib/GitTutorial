7b10beb4e9f9f4f59b6bc11dc7ac7a5ae92d698d
Rambo <183870794@qq.com>
2023-04-30 13:11:17 +0800

    add 6nd line

--- test1.cpp
+++ test1.cpp
@@ -6,5 +6,6 @@ int main(){
     std::cout<<"hah"<<std::endl;
     std::cout<<"hahaaaaa4"<<std::endl;
     std::cout<<"hahaaaaa5"<<std::endl;
+    std::cout<<"hahaaaaa6"<<std::endl;
     return 0;
 }
